0c4ce0c6645505b814ff426277b4f74724da03f2
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-02-27 19:55:17 +0100

    comit 2

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,6 +13,7 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public BaseGradeBook Type { get; set; }
+        
         public List<Student> Students { get; set; }
 
         public BaseGradeBook(string name)
