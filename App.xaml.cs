0eb2834181653e6fe723c48709d15bca54d1b69e
Piotr Bacior <piotr.bacior@microsoft.wsei.edu.pl>
2025-03-15 21:30:49 +0100

    commit 15

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.Windows;
 
+//nazwa przestrzeni nazw GradeBookApp
 namespace GradeBookApp
 {
+    //klasa App dziedzicząca po klasie Application
     public partial class App : Application
     {
     }
